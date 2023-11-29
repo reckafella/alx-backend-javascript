7aee65d0ec636cd989dcb7d3a9ed5af0179a85f1
reckafella <ethanmuthoniw6@gmail.com>
2023-11-29 22:30:51 +0300

    update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.all([signUpUser(firstname, lastname), uploadPhoto(filename)])
+  return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
     .then((result) => console.log(result));
 }
