1ab65897c5b102dcd3423fbefa89a93a2db7f5b5
reckafella <ethanmuthoniw6@gmail.com>
2023-11-29 21:50:54 +0300

    update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  const errorObject = new Error('Signup system offline');
-
-  return Promise.all([photo, user])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((result) => console.log(result[0].body, result[1].firstName, result[1].lastName))
-    .catch(() => console.log(errorObject));
+    .catch(() => console.log('Signup system offline'));
 }
