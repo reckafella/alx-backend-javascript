c64d0e827dc0d2a60e53411c563758790824ef09
reckafella <ethanmuthoniw6@gmail.com>
2023-11-29 22:47:04 +0300

    fvkn unbelievable

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
+export default async function handleProfileSignup(firstname, lastname, filename) {
   return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
     .then((result) => {
       result.map((data) => ({
