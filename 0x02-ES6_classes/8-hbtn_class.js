3b0528cc6b84be55ebe487cd80b14be9cbbe8345
reckafella <ethanmuthoniw6@gmail.com>
2023-12-02 01:57:58 +0300

    update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,4 +1,4 @@
-export default class HolbertonClass{
+export default class HolbertonClass {
   constructor(size, location) {
     if (typeof size !== 'number') {
       throw new TypeError('size should be a number');
