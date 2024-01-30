3d2d0aa5a7a1b538fc18f48a4a914c99408e5450
Reckafella <ethanmuthoni@gmail.com>
2024-01-30 14:05:27 +0300

    update task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+/* const readline = require('readline');
 
 readline.createInterface({
   input: process.stdin,
@@ -13,4 +13,19 @@ readline.on('line', (name) => {
 
 readline.on('close', () => {
   console.log('This important software is now closing');
+}); */
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
