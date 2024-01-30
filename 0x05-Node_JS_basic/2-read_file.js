7a9507e1dbac044722aa0be8a9a0ff38aa62b6ee
Reckafella <ethanmuthoni@gmail.com>
2024-01-30 15:57:55 +0300

    code refactoring

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,24 @@
 const fs = require('fs');
 
+function groupStudentsByField(students, indexField, indexFirstName) {
+  const fields = {};
+  for (const student of students) {
+    if (!fields[student[indexField]]) fields[student[indexField]] = [];
+    fields[student[indexField]].push(student[indexFirstName]);
+  }
+  return fields;
+}
+
+function printStudentSummary(totalStudents, fields) {
+  // print total number of students
+  console.log(`Number of students: ${totalStudents}`);
+
+  // print number and list of students in each field
+  Object.entries(fields).forEach(([key, value]) => {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  });
+}
+
 function countStudents(path) {
   if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
@@ -7,11 +26,7 @@ function countStudents(path) {
 
   const data = fs.readFileSync(path, 'utf8');
   const lines = data.trim().split('\n');
-  const students = [];
-
-  for (const line of lines) {
-    students.push(line.trim().split(','));
-  }
+  const students = lines.map((line) => line.trim().split(','));
 
   const headers = students[0];
   students.shift();
@@ -20,19 +35,9 @@ function countStudents(path) {
   const indexField = headers.indexOf('field');
 
   // object to hold the list of students' first names, grouped by field
-  const fields = {};
-  for (const student of students) {
-    if (!fields[student[indexField]]) fields[student[indexField]] = [];
-    fields[student[indexField]].push(student[indexFirstName]);
-  }
+  const fields = groupStudentsByField(students, indexField, indexFirstName);
 
-  // print total number of students
-  console.log(`Number of students: ${students.length}`);
-
-  // print number and list of students in each field
-  Object.entries(fields).forEach(([key, value]) => {
-    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-  });
+  printStudentSummary(students.length, fields);
 }
 
 module.exports = countStudents;
