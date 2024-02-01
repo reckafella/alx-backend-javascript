7f8cd09c3139955d98b0fc374e823241a099998f
Reckafella <ethanmuthoni@gmail.com>
2024-02-01 17:38:12 +0300

    add reminder notes

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,6 +9,7 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
+  // test skipped. Marked as pending
   it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
